18a61d63801bda0d687dbccbceb23ae34a653f75
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2021-11-13 20:10:53 +0700

    feat: add emergency service activity

---
+++ app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/EmergencyServiceActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.if570.titik.koma.klikdarurat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmergencyServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_service);
+    }
+}
