6c78752678df265722f7fffeada25002b096369a
Dimas Lesmana <35789873+dimaslesmana@users.noreply.github.com>
2021-11-17 05:50:35 +0700

    feat: add bottom navigation
    add bottom navigation
    change emergency service activity to fragment
    change personal emergency contact to fragment
    change emergency service location to fragment
    change user profile activity to fragment

--- app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/EditUserProfileActivity.java
+++ app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/EditUserProfileActivity.java
@@ -2,6 +2,7 @@ package id.ac.umn.if570.titik.koma.klikdarurat;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditUserProfileActivity extends AppCompatActivity {
@@ -9,5 +10,17 @@ public class EditUserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user_profile);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.edit_user_profile_tv_title);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
--- app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/MainActivity.java
+++ app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/MainActivity.java
@@ -1,14 +1,59 @@
 package id.ac.umn.if570.titik.koma.klikdarurat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new EmergencyServiceFragment());
+
+        BottomNavigationView navView = findViewById(R.id.bottom_nav_main);
+        navView.setOnItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment == null) {
+            return false;
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_nav_main, fragment).commit();
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.nav_menu_emergency_service:
+                selectedFragment = new EmergencyServiceFragment();
+                break;
+            case R.id.nav_menu_personal_emergency_contact:
+                selectedFragment = new PersonalEmergencyContactFragment();
+                break;
+            case R.id.nav_menu_emergency_service_location:
+                selectedFragment = new EmergencyServiceLocationFragment();
+                break;
+            case R.id.nav_menu_user_profile:
+                selectedFragment = new UserProfileFragment();
+                break;
+        }
+
+        return loadFragment(selectedFragment);
     }
 }
