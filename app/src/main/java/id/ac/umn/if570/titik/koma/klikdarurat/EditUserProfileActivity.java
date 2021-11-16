691b5928e041b1074c854f8185dd140319863efe
Ade Kiswara <ade.kiswara@student.umn.ac.id>
2021-11-16 17:46:33 +0700

    feat: add content in profile and edit profile page

---
+++ app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/EditUserProfileActivity.java
@@ -0,0 +1,13 @@
+package id.ac.umn.if570.titik.koma.klikdarurat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditUserProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_profile);
+    }
+}
---
+++ app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/UserProfileActivity.java
@@ -0,0 +1,13 @@
+package id.ac.umn.if570.titik.koma.klikdarurat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
