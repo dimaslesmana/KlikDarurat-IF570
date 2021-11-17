0eb21cb9876c662046d0bcbc61f8beb24f0860b0
Dimas Lesmana <35789873+dimaslesmana@users.noreply.github.com>
2021-11-17 09:05:30 +0700

    refactor: change maps using fragment

--- app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/EmergencyServiceLocationFragment.java
+++ app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/EmergencyServiceLocationFragment.java
@@ -2,18 +2,27 @@ package id.ac.umn.if570.titik.koma.klikdarurat;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link EmergencyServiceLocationFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EmergencyServiceLocationFragment extends Fragment {
+public class EmergencyServiceLocationFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap googleMap;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +68,19 @@ public class EmergencyServiceLocationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_emergency_service_location, container, false);
 
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+        getChildFragmentManager().beginTransaction().replace(R.id.fragment_emergency_service_location_map, mapFragment).commit();
+        mapFragment.getMapAsync(this);
+
         return view;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        LatLng coordinate = new LatLng(-6.257324602410285, 106.61834152594922);
+        this.googleMap.addMarker(new MarkerOptions().position(coordinate).title("Test"));
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(coordinate));
     }
 }
