c2400f804f2e24b52445e23e278ee0fd5683ab59
Muhamad fajri <muhamad.fajri@student.umn.ac.id>
2021-11-29 09:49:40 +0700

    commit

--- app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/RegisterActivity.java
+++ app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/RegisterActivity.java
@@ -1,17 +1,31 @@
 package id.ac.umn.if570.titik.koma.klikdarurat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnRegister;
     private TextView tvLoginAccount;
+    private FirebaseAuth mAuth;
+    private EditText editTextfull_name, editTextemail, editTextpassword;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,14 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
         btnRegister.setOnClickListener(this);
         tvLoginAccount.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextfull_name = (EditText) findViewById(R.id.full_name);
+        editTextemail = (EditText) findViewById(R.id.email);
+        editTextpassword = (EditText) findViewById(R.id.password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
     }
 
     @Override
@@ -38,5 +60,72 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private void initView() {
         btnRegister = findViewById(R.id.button_register_register);
         tvLoginAccount = findViewById(R.id.tv_register_login_account);
+
+        String email= editTextemail.getText().toString().trim();
+        String password= editTextpassword.getText().toString().trim();
+        String full_name= editTextfull_name.getText().toString().trim();
+
+        if (full_name.isEmpty()){
+            editTextfull_name.setError("Full name is required!");
+            editTextfull_name.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()){
+            editTextemail.setError("Email is required!");
+            editTextemail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextemail.setError("Please provide valid email");
+            editTextemail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            editTextpassword.setError("Password is required!");
+            editTextpassword.requestFocus();
+            return;
+        }
+        if (password.length() <6){
+            editTextpassword.setError("Min Password 6 Karakter!");
+            editTextpassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User user = new User(full_name, email);
+
+                            String path;
+                            FirebaseDatabase.getInstance().getReference( "Users")
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(RegisterActivity.this, "User has been registered successfully!", Toast.LENGTH_LONG).show();
+                                                progressBar.setVisibility(View.GONE);
+
+                                                //redirect to login layout!
+                                            } else {
+                                                Toast.makeText(RegisterActivity.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                                                progressBar.setVisibility(View.GONE);
+
+                                            }
+                                        }
+
+                                    });
+                                }else {
+                            Toast.makeText(RegisterActivity.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
     }
 }
---
+++ app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/User.java
@@ -0,0 +1,13 @@
+package id.ac.umn.if570.titik.koma.klikdarurat;
+
+public class User {
+
+    public String full_name, email;
+    public User(){
+
+    }
+    public  User(String full_name,String email){
+        this.full_name = full_name;
+        this.email = email;
+    }
+}
