90a7c979f5bb4ad4f51764f32f2b52af90239b73
Dimas Lesmana <35789873+dimaslesmana@users.noreply.github.com>
2021-12-01 04:46:07 +0700

    feat: add personal emergency contact

---
+++ app/src/main/java/id/ac/umn/if570/titik/koma/klikdarurat/AddPersonalEmergencyContactActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.if570.titik.koma.klikdarurat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPersonalEmergencyContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_personal_emergency_contact);
+    }
+}
